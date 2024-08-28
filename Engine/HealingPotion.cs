a96e3ebf013e4047c6d4398f3de884693cc15010
Owen Oliveira <ooliveira9359@gmail.com>
2024-08-27 23:54:29 -0400

    working

--- Engine/HealingPotion.cs
+++ Engine/HealingPotion.cs
@@ -8,10 +8,14 @@ namespace Engine
     public class HealingPotion : Item
     {
         public int AmountToHeal { get; set; }
+        int cry = 100;
+        int why = 0;
+
 
         public HealingPotion(int id, string name, string namePlural, int amountToHeal) : base(id, name, namePlural)
         {
             AmountToHeal = amountToHeal;
         }
     }//if this works ill cry
+    //test commit
 }
